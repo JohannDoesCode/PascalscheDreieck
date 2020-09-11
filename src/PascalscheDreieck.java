a3fdc35387f16b4d605b4fbefb8491c6603cc5bb
JohannDoesCode <johann@cerkovnik.de>
2020-09-11 15:44:11 +0200

    get

--- src/PascalscheDreieck.java
+++ src/PascalscheDreieck.java
@@ -6,6 +6,9 @@ public class PascalscheDreieck {
         int[][] pascal = new int[row][];
         for(int i = 0; i < pascal.length ; i++){
             pascal[i] = new int[i+1];
+            for (int s = pascal.length;s >= i ;s-- ) {
+                  System.out.print(" ");
+            } 
             for(int j = 0; j < i; j++){
                 if(j == 0 || j == i){
                     pascal[i][j] = 1;
@@ -14,23 +17,22 @@ public class PascalscheDreieck {
                     pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
                 }
                 System.out.print(pascal[i][j] + " ");
-            }
+            } 
             System.out.println("");
         }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("------------------------------------------------------");
-        System.out.println("Enter the maximum row number: ");
-        System.out.println("------------------------------------------------------");
-        row = scanner.nextInt();
-        new PascalscheDreieck(row+1);
+        recreate();
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  
+  public static void recreate(){
+    Scanner scanner = new Scanner(System.in);
         System.out.println("------------------------------------------------------");
         System.out.println("Enter the maximum row number: ");
         System.out.println("------------------------------------------------------");
         int row = scanner.nextInt();
         new PascalscheDreieck(row+1);
+    }
+  
+    public static void main(String[] args) {
+        recreate();
     }
 }
